6385e98012492b46f519b6b7a56cdcadde4b1219
vilisperis <vilisperis@gmail.com>
2019-10-08 14:32:46 +0300

    papildymas

--- main.py
+++ main.py
@@ -1,3 +1,22 @@
 input_path_file = "pdf"
 international_words = ["kein tausch", "palettenshein", 'status', "cmr", "parking", "waiting time", "penalty", "tauschen", "safety"]
+import PyPDF2 
+import textract
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
+filename = 'uzsakymas'
+pdfFileObj = open(filename,'rb') 
+pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+num_pages = pdfReader.numPages
+count = 0
+text = ""
+while count < num_pages:
+    pageObj = pdfReader.getPage(count)
+    count +=1
+    text += pageObj.extractText()
+if text != "":
+   text = text
+else:
+   text = textract.process(fileurl, method='tesseract', language='eng')
+print(text)
