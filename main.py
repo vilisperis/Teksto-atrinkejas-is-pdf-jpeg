eb61fe924f05e5d8ddacae3295e377c8151e02dc
vilisperis <vilisperis@gmail.com>
2019-10-09 22:42:45 +0300

    papild

--- main.py
+++ main.py
@@ -1,35 +1,34 @@
 import os
 os.chdir("C:\\Users\\Pc")
 
-international_words = "Palettenschein"
-filename = 'viso.pdf' 
-pdfFileObj = open(filename,'rb')
-print(pdfFileObj)
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
-print(pdfReader)
-num_pages = pdfReader.numPages
-print(num_pages)
-count = 0
-text = ""
-while count < num_pages:
-    pageObj = pdfReader.getPage(count)
-    count += 1
-    text += pageObj.extractText()
-    print(text)
-if text != "":
-    text = text
-else:
-   text = textract.process("fileurl", method='tesseract', language='eng')
-print(text)
+import PyPDF2 
+import textract
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
 
-tokens = word_tokenize(text)
-stop_words = stopwords.words('english')
-keywords = [word for word in tokens if not word in stop_words and not word in string.punctuation]
+filename = 'viso.pdf'
+search_for = "Palettenschein"
+def searchInPDF(filename, key):
+    occurrences = 0
+    pdfFileObj = open(filename,'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    num_pages = pdfReader.numPages
+    count = 0
+    text = ""
+    while count < num_pages:
+        pageObj = pdfReader.getPage(count)
+        count +=1
+        text += pageObj.extractText()
+    if text != "":
+       text = text
+    else:
+       text = textract.process(filename, method='tesseract', language='eng')
+    tokens = word_tokenize(text)
+    punctuation = ['(',')',';',':','[',']',',']
+    stop_words = stopwords.words('english')
+    keywords = [word for word in tokens if not word in stop_words and  not word in punctuation]
+    for k in keywords:
+        if key == k: occurrences+=1
+    return occurrences 
 
-for i in range(0, num_pages):
-    pageObj = pdfReader.getPage(i)
-    print("this is page " + str(i)) 
-    text = pageObj.extractText() 
-    print(text)
-    ResSearch = re.search(international_words, text)
-    print(ResSearch)
+print(searchInPDF(filename, search_for))
