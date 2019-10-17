d19e5b7342e66949ab578291572c41136388858b
Aidis Stukas <aidiss@gmail.com>
2019-10-17 20:01:52 +0300

    Fix #3

--- main.py
+++ main.py
@@ -9,6 +9,15 @@ from nltk.corpus import stopwords
 filename = 'viso.pdf'
 search_for_words = "Palettenschein"
 def searchInPDF(filename, search_for_words):
+    """Looks for tokens in pdf files.
+    
+    Uses PyPDF if it fails textract(tesseract) is used
+
+    filename - path to input file
+    search_for_words (list) - tokens to look for
+
+    Returns: occurances
+    """
     occurrences = 0
     pdfFileObj = open(filename,'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
@@ -27,7 +36,7 @@ def searchInPDF(filename, search_for_words):
     punctuation = ['(',')',';',':','[',']',',']
     stop_words = stopwords.words('english')
     keywords = [word for word in tokens if not word in stop_words and  not word in punctuation]
-    for k in keywords:
+    for k in keywords:"""This functions prints its arg
         if search_for == k: occurrences+=1
     return occurrences 
 tokens = ['technisch', "pallet", "waiting times", "pauschal", "Palettentausch", "minimum", "original", "cmr", "signature", "(EP)" ]
